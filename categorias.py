8161b278dd43e04617fa8e14ce4382cb24e971cc
Antonio Rafael Cedillo Rodríguez <116749018+AntonioCedilloA01656823@users.noreply.github.com>
2023-03-26 18:19:36 -0600

    toques finales

---
+++ categorias.py
@@ -0,0 +1,83 @@
+#Tipos de datos ---
+
+num = 9 #int
+decimal = 9.5 #float
+booleano = True #bool
+texto = "Holi" #string
+
+#Comentarios --
+
+#
+
+
+#Operadores---
+
+#Operadores matematicos---
+#+
+#-
+#/
+#//
+#%
+#**
+#=
+#+=
+#-=
+
+#Operadores booleanos---
+#==
+#!=
+#<
+#>
+#<=
+#>=
+#and
+#or
+#not
+
+#Caracteres especiales---
+
+# #
+# :
+# ,
+# .
+# []
+# ()
+# {}
+
+#Palabras reservadas---
+# global
+#public
+#class
+#break
+#continue
+#import
+#None
+#in
+#def
+#from
+#pass
+#raise
+#nonlocal
+
+#condicionales----
+
+if():
+elif():
+else():
+
+
+#ciclos
+
+for cond:
+
+while cond:
+
+
+
+#Metodos
+def hola ():
+
+lambda
+
+return
+
---
+++ tiposC++.cpp
@@ -0,0 +1,107 @@
+
+
+//Tipos de datos---
+
+int num = 1; //Entero
+float num2 = 9.5; //Float
+double num3 = 100.24; //Double
+
+char letra = 'a'; //Letra
+String texto = "holi"; //string
+
+bool bolean = true; //booleano, puede ser falso tambien
+
+
+//Operadores---
+
+//Matematicos
+
+/* +
+-
+*
+/
+%
++=
+-=
+*=
+/=
+%=
+++
+--
+
+//booleanos
+||
+&&
+<
+>
+==
+<=
+>=
+
+// otros
+
+<<
+>>
+~
+| */
+
+
+//Comentarios ---
+
+//
+
+//Condicionales ---
+
+if (/* condition */)
+{
+    /* code */
+} else if (/* condition */)
+{
+    /* code */
+} else {
+
+}
+
+switch (expression)
+{
+
+
+case /* constant-expression */:
+    /* code */
+    break;
+
+default:
+    break;
+}
+
+
+
+//Ciclos ---
+
+for (size_t i = 0; i < count; i++)
+{
+    /* code */
+}
+
+while (/* condition */)
+{
+    /* code */
+}
+
+
+
+//Metodos ---
+
+public string hola (){
+    return "hola";
+}
+
+
+//Palabras reservadas ---
+
+// public
+// private
+// return
+// void
+// continue;
+// break;
